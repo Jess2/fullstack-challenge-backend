1eef791bf7a3c0ce8ebba6a17c339e24703c4879
JESS2 <jess2.developer@gmail.com>
2021-07-22 15:37:03 +0900

    User 생성시 username, password length validation 추가

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -30,18 +30,36 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [{
+                    field: "username",
+                    message: "length must be greater than 2"
+                }]
+            }
+        }
+
+        if (options.password.length <= 3) {
+            return {
+                errors: [{
+                    field: "password",
+                    message: "length must be greater than 3"
+                }]
+            }
+        }
+
         const hashedPassword = await argon2.hash(options.password);
         const user = em.create(User, { 
             username: options.username, 
             password: hashedPassword,
         });
         await em.persistAndFlush(user);
-        return user;
+        return { user };
     }
 
     @Mutation(() => UserResponse)
@@ -68,8 +86,6 @@ export class UserResolver {
             };
         }
 
-        return {
-            user,
-        };
+        return { user };
     }
 }
